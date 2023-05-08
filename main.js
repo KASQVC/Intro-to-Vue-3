d5a6b24728cc3a37f236eb31a835f13edd4f9918
KASQVC <KaRon.Scott@qvc.com>
2023-05-08 14:00:51 -0400

    Computed components

--- main.js
+++ main.js
@@ -1,15 +1,54 @@
 const app = Vue.createApp({
     data() {
         return {
-            cart:0,
+            cart: 0,
+            onSale: true,
             product: 'Socks',
-            image: './assets/images/socks_blue.jpg',
-            inStock: true,
+            brand: 'Vue Mastery',
+            selectedVariant: 0,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
+              { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
+              { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
             ]
+        }
+    },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        updateVariant(index){
+            this.selectedVariant = index
+        },
+        deleteFromCart(){
+            if(this.cart == 0){
+                return;
+            }else{
+                this.cart -= 1
+            }
+        },
+        currentStockChange(currentStock){
+            if(this.cart == 5){
+                inStock = false;
+            }
+            inStock = currentStock
+        }
+    },
+    computed: {
+        title () {
+            return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
+        },
+        sale(){
+            if(this.onSale){
+                return this.title + ' is on sale.'
+            }
+            return ''
         }
     }
 })
